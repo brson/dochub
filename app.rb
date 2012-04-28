8c7aa4239cd0ed3df3d8d9af88cd54ee2da23b80
Brian Anderson <banderson@mozilla.com>
2012-04-28 15:52:45 -0700

    Use rack

---
+++ app.rb
@@ -0,0 +1,31 @@
+require 'sinatra/base'
+require 'gollum'
+require 'mustache/sinatra'
+
+class App < Sinatra::Base
+  register Mustache::Sinatra
+
+  set :mustache, {
+    :tempaltes => "./templates",
+    :views => "./views"
+  }
+
+  get '/' do
+    'Hello World'
+  end
+
+  get '/:user/:repo' do
+    show_page_or_file(params[:user], params[:repo], 'Home')
+  end
+
+  def show_page_or_file(user, repo, name)
+    dir = @dir
+    wiki = Gollum::Wiki.new("./data/#{user}/#{repo}.wiki")
+    if page = wiki.page(name)
+      @page = page
+      @name = name
+      @content = page.formatted_data
+      return @content
+    end
+  end
+end
