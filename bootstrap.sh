6e7a3ef25616b12a91ca671030862facbe5ff073
Brian Anderson <banderson@mozilla.com>
2012-05-28 17:47:30 -0700

    Add -y to apt-get command

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd && \
-sudo apt-get install git rubygems1.8 ruby-bundler libxml2-dev libxslt1-dev && \
+sudo apt-get install -y git rubygems1.8 ruby-bundler libxml2-dev libxslt1-dev && \
 git clone git://github.com/brson/dochub && \
 cd dochub && \
 sudo bundler install && \
