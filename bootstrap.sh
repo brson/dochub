de5c5529531c9fdb08ceb39df43cd1aa6acbcc65
Brian Anderson <banderson@mozilla.com>
2012-05-28 17:49:08 -0700

    Typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+# execute with
+# wget -qO- https://raw.github.com/brson/dochub/master/bootstrap.sh | sudo -u ubuntu sh
+
 cd && \
 sudo apt-get install -y git rubygems1.8 ruby-bundler libxml2-dev libxslt1-dev && \
 git clone git://github.com/brson/dochub && \
 cd dochub && \
-sudo bundler install && \
+sudo bundle install && \
 rackup &
